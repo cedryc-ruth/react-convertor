0e95d66bffe8b8654a9c5ad3f17cfc8cdadcc95e
Cédryc Ruth <ceruth@domepfc.net>
2021-11-19 15:02:27 +0100

    JSX Main App structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import './App.css';
 
 function App() {
   return (
-
+    <>
+      <h1>Convert</h1>
+      <div>
+        <input type="number" />
+        <select>
+          <option value="EUR">EUR</option>
+          <option value="YEN">YEN</option>
+        </select>
+      </div>
+      <div>=</div>
+      <div>
+        <input type="number" />
+        <select>
+          <option value="EUR">EUR</option>
+          <option value="YEN">YEN</option>
+        </select>
+      </div>
+    </>
   );
 }
 
